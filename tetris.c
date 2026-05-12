920220d6036dca653dee59f9d79d7fe90bbb90f7
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-12 01:47:48 +0000

    Função remover da fila

--- tetris.c
+++ tetris.c
@@ -120,3 +120,11 @@ void adicionarAFila(Fila *f, int *proximoId) {
         f->total++;
     }
 }
+
+// Função remover da fila
+Peca removerDaFila(Fila *f) {
+    Peca p = f->itens[f->inicio];
+    f->inicio = (f->inicio + 1) % MAX_FILA;
+    f->total--;
+    return p;
+}
