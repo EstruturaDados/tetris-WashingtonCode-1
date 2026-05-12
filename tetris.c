67a998f551339b62f8f53e70693f388526478b02
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-12 01:44:11 +0000

    Estrutura da Fila Circular (Peças Futuras)

--- tetris.c
+++ tetris.c
@@ -16,3 +16,11 @@ typedef struct {
     int id;    // Identificador único
 } Peca;
 
+// Estrutura da Fila Circular (Peças Futuras)
+typedef struct {
+    Peca itens[MAX_FILA];
+    int inicio;
+    int fim;
+    int total;
+} Fila;
+
