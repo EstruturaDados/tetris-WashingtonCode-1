616bf534be65800da7cc25d9f30be4c5e59b5b8f
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-12 01:43:02 +0000

    Inclusão de bibliotecas necessárias

--- tetris.c
+++ tetris.c
@@ -1,2 +1,8 @@
 // Desafio Tetris Stack - Nível Aventureiro
 // Objetivo: Implementar um programa em C que simula o gerenciamento de peças no jogo Tetris Stack, utilizando uma fila circular de peças futuras e uma pilha de reserva. Cada peça tem um tipo (como 'I', 'O', 'T', 'L') e um identificador numérico único (id). O jogador poderá visualizar a fila e a pilha, jogar peças, reservar peças para uso posterior e remover peças da reserva.
+
+// Inclusão de bibliotecas necessárias
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
