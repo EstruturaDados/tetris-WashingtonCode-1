3e78b2b667694380a80cc3f9b16e72b89c21e9fc
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-12 01:43:47 +0000

    Estrutura para representar uma peça

--- tetris.c
+++ tetris.c
@@ -9,3 +9,10 @@
 // Definição de constantes
 #define MAX_FILA 5
 #define MAX_PILHA 3
+
+// Estrutura para representar uma peça
+typedef struct {
+    char nome; // 'I', 'O', 'T', 'L'
+    int id;    // Identificador único
+} Peca;
+
